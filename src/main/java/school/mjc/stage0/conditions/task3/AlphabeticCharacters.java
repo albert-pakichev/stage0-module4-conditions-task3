07217934b09b8f8fb074520dff84a8a397d5e6ae
albert-pakichev <69592416+albert-pakichev@users.noreply.github.com>
2023-06-28 21:09:57 +0500

    Update AlphabeticCharacters.java

--- src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
+++ src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
@@ -2,5 +2,14 @@ package school.mjc.stage0.conditions.task3;
 
 public class AlphabeticCharacters {
     public void vowelDeterminer(char character) {
+        if ("AEIOUaeiou".indexOf(symbol) != -1)
+ {
+        System.out.println("Vowel");
+    } else if ("QWRTYPSDFGHJKLZXCVBNMqwrtypsdfghjklzxcvbnm".indexOf(symbol) != -1) {
+        System.out.println("Consonant");
+    }
+    else {
+        System.out.println("wrong alphabet!");
+    }
     }
 }
